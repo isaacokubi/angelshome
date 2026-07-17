baba2f0d87b114beb15792bf112b24da7d01e60b
Barack Okubi <izobrack3@gmail.com>
2026-07-17 16:00:53 +0300

    Update Angels Home website

--- client/src/components/SocialLinks.jsx
+++ client/src/components/SocialLinks.jsx
@@ -1,15 +1,30 @@
 export default function SocialLinks() {
   return (
     <div className="flex gap-5 mt-6">
-      <a href="https://facebook.com/josephat.okama" className="bg-blue-700 text-white px-5 py-3 rounded-lg">
+      <a
+        href="https://facebook.com/josephat.okama"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-blue-700 text-white px-5 py-3 rounded-lg"
+      >
         Facebook
       </a>
 
-      <a href="#" className="bg-black text-white px-5 py-3 rounded-lg">
+      <a
+        href="https://x.com/josephatAndula"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black text-white px-5 py-3 rounded-lg"
+      >
         X
       </a>
 
-      <a href="#" className="bg-red-600 text-white px-5 py-3 rounded-lg">
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-red-600 text-white px-5 py-3 rounded-lg"
+      >
         YouTube
       </a>
     </div>
