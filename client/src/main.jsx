5d70e396434ab3fed2713e90890270b458e1cc4f
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-24 23:29:44 +0300

    Remove application-level deployment password gate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import PrivateGate from "./components/PrivateGate";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <PrivateGate>
-        <App />
-      </PrivateGate>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
