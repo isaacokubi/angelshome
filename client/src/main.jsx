722bd950bc21f81abff8f2449755f4470b38f1b3
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-26 01:08:57 +0300

    feat: apply school theme globally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { SchoolSettingsProvider } from "./context/SchoolSettingsContext";
+import ThemeProvider from "./components/ThemeProvider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <SchoolSettingsProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </SchoolSettingsProvider>
     </BrowserRouter>
   </React.StrictMode>,
