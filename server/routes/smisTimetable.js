451de6b597275ff5b6d93da5cd8a053ad8594d1a
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-25 18:41:56 +0300

    feat: expose timetable generation endpoint

--- server/routes/smisTimetable.js
+++ server/routes/smisTimetable.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { requireSchoolAuth, requireSchoolRole } = require('../middleware/schoolAuth');
-const { listTimetable, createTimetable, updateTimetable, deleteTimetable } = require('../controllers/smisTimetableController');
+const { listTimetable, createTimetable, updateTimetable, deleteTimetable, generateTimetable } = require('../controllers/smisTimetableController');
 
 const router = express.Router();
 const adminOnly = requireSchoolRole('admin');
 
 router.get('/', requireSchoolAuth, listTimetable);
+router.post('/generate', requireSchoolAuth, adminOnly, generateTimetable);
 router.post('/', requireSchoolAuth, adminOnly, createTimetable);
 router.patch('/:id', requireSchoolAuth, adminOnly, updateTimetable);
 router.delete('/:id', requireSchoolAuth, adminOnly, deleteTimetable);
