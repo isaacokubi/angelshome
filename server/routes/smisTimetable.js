8cb8d8be5edf8ee81a2424b38b7e44a14f1c07aa
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-25 19:49:32 +0300

    feat: expose saved timetable configuration

--- server/routes/smisTimetable.js
+++ server/routes/smisTimetable.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const { requireSchoolAuth, requireSchoolRole } = require('../middleware/schoolAuth');
-const { listTimetable, createTimetable, updateTimetable, deleteTimetable, generateTimetable } = require('../controllers/smisTimetableController');
-const { listScopedTimetable } = require('../controllers/scopedTimetableController');
+const express = require("express");
+const { requireSchoolAuth, requireSchoolRole } = require("../middleware/schoolAuth");
+const { listTimetable, getTimetableConfig, createTimetable, updateTimetable, deleteTimetable, generateTimetable } = require("../controllers/smisTimetableController");
+const { listScopedTimetable } = require("../controllers/scopedTimetableController");
 
 const router = express.Router();
-const adminOnly = requireSchoolRole('admin');
+const adminOnly = requireSchoolRole("admin");
 
-router.get('/', requireSchoolAuth, listScopedTimetable);
-router.post('/generate', requireSchoolAuth, adminOnly, generateTimetable);
-router.post('/', requireSchoolAuth, adminOnly, createTimetable);
-router.patch('/:id', requireSchoolAuth, adminOnly, updateTimetable);
-router.delete('/:id', requireSchoolAuth, adminOnly, deleteTimetable);
+router.get("/", requireSchoolAuth, listScopedTimetable);
+router.get("/config", requireSchoolAuth, adminOnly, getTimetableConfig);
+router.post("/generate", requireSchoolAuth, adminOnly, generateTimetable);
+router.post("/", requireSchoolAuth, adminOnly, createTimetable);
+router.patch("/:id", requireSchoolAuth, adminOnly, updateTimetable);
+router.delete("/:id", requireSchoolAuth, adminOnly, deleteTimetable);
 
 module.exports = router;
