aee5985b02de3555ee66e0cddd8b9db0cf15ae87
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-25 18:57:33 +0300

    feat: enforce role-scoped timetable reads

--- server/routes/smisTimetable.js
+++ server/routes/smisTimetable.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { requireSchoolAuth, requireSchoolRole } = require('../middleware/schoolAuth');
 const { listTimetable, createTimetable, updateTimetable, deleteTimetable, generateTimetable } = require('../controllers/smisTimetableController');
+const { listScopedTimetable } = require('../controllers/scopedTimetableController');
 
 const router = express.Router();
 const adminOnly = requireSchoolRole('admin');
 
-router.get('/', requireSchoolAuth, listTimetable);
+router.get('/', requireSchoolAuth, listScopedTimetable);
 router.post('/generate', requireSchoolAuth, adminOnly, generateTimetable);
 router.post('/', requireSchoolAuth, adminOnly, createTimetable);
 router.patch('/:id', requireSchoolAuth, adminOnly, updateTimetable);
