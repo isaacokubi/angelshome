bbd001e06960feaeaaa052572430740d73c6ade4
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-26 00:42:42 +0300

    feat: expose public school settings endpoint

--- server/routes/cms.js
+++ server/routes/cms.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const settingsController = require("../controllers/settingsController");
+
+router.get("/settings", settingsController.getPublicSettings);
 
 router.get("/staff", (req, res) => {
   res.json([]);
