f9d259fede123084da1b0fea788684480f39e2cd
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-18 20:03:53 +0300

    feat: add teacher signature storage

--- server/models/TeacherProfile.js
+++ server/models/TeacherProfile.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const teacherProfileSchema = new mongoose.Schema({
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true, index: true },
   teacherCode: { type: String, required: true, trim: true, uppercase: true, maxlength: 30, unique: true, index: true },
+  signatureUrl: { type: String, trim: true, default: "", maxlength: 2000 },
 }, { timestamps: true });
 
 module.exports = mongoose.model("TeacherProfile", teacherProfileSchema);
